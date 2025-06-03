418908469ac406798cd9049c58ba8268c3975d07
Katheryn-Caceres-S <kcs.ingcivil@gmail.com>
2025-06-03 00:55:17 +0000

    ejercicio 2

--- word_count.py
+++ word_count.py
@@ -2,13 +2,26 @@
 import sys
 
 if len(sys.argv) != 2:
-    print("python ejemplo.py 'celsius'")
+    print("python word_count.py 'celsius'")
     sys.exit(1)
 
 archivo = sys.argv[1]
 
 #leer archivo
 # "r" es de read leer
+#El número de caracteres distintos es: 40
+#El número de palabras distintas es: 243
 
 with open(archivo, "r", encoding="utf-8") as f:
-    print(f.read())
+    texto = f.read()
+
+
+total_palabras = texto.split()
+
+letras_distintas = set(texto)
+palabras_distintas = set(total_palabras)
+
+
+print("El número de caracteres distintos es de: ", len(letras_distintas))
+print("El número de palabras distintas es de: ", len(palabras_distintas))
+
